076b3a6bfe43c820696b6499b71aea0f627a8108
Gerson David Soto <51761203+Gdsoto@users.noreply.github.com>
2021-03-11 19:06:51 -0500

    Add 01

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,14 @@
+	section .data
+msg:		 db "Hello, Holberton", 10 ;
+	section .text
+	global main
+main:
+	mov rdx, 17
+	mov rcx, msg
+	mov rbx, 1
+	mov rax, 4
+	int 0x80
+
+	mov rbx, 0
+	mov rax, 1
+	int 0x80
